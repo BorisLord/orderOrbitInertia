a1214c36e621c5eb202ae8137deb892fc9ef7309
BorisPad <borisdelord@proton.me>
2025-01-09 00:59:28 +0100

    add createExchange and trimBalance method

--- app/services/broker_service.ts
+++ app/services/broker_service.ts
@@ -1,5 +1,5 @@
 import { ApiKeyPick } from '#models/api_key'
-import ccxt, { Exchange, exchanges, pro } from 'ccxt'
+import ccxt, { Balances, Exchange, exchanges, pro } from 'ccxt'
 
 export class BrokerService {
   static validateExchangeId(exchangeId: string): boolean {
@@ -36,5 +36,32 @@ export class BrokerService {
       console.error(`Erreur de connexion au broker '${data.exchangeId}':`, error.message)
       throw new Error(`Connexion au broker '${data.exchangeId}' impossible. Vérifiez vos clés API.`)
     }
+  }
+
+  static createExchange(apiKeys: ApiKeyPick): Exchange {
+    const exchangeClass = (ccxt as any)[apiKeys.exchangeId]
+    if (!exchangeClass) {
+      throw new Error(`L'exchange ${apiKeys.exchangeId} n'est pas supporté`)
+    }
+
+    const exchange: Exchange = new exchangeClass({
+      apiKey: apiKeys.apiKey,
+      secret: apiKeys.secret,
+      enableRateLimit: true,
+    })
+
+    return exchange
+  }
+
+  static trimBalance(balance: Balances) {
+    return Object.keys(balance.total)
+      .filter((key) => key !== 'info' && key !== 'timestamp' && key !== 'datetime') // Ignorer les clés spéciales
+      .map((asset) => ({
+        asset,
+        free: balance[asset].free || 0,
+        used: balance[asset].used || 0,
+        total: balance[asset].total || 0,
+      }))
+      .filter(({ free, used, total }) => free !== 0 || used !== 0 || total !== 0) // Supprime les tokens inutiles
   }
 }
