6b8feec6ef01e5ed9bc33318418ccdd3b57d5f41
BorisPad <borisdelord@proton.me>
2025-01-09 01:00:19 +0100

    add ability to retrive secretKey in getApiKeysByUser

--- app/services/api_key_service.ts
+++ app/services/api_key_service.ts
@@ -1,4 +1,5 @@
 import ApiKey, { ApiKeyPick } from '#models/api_key'
+import encryption from '@adonisjs/core/services/encryption'
 
 export class ApiKeyService {
   static async createApiKey(data: ApiKeyPick, userId: number) {
@@ -8,10 +9,31 @@ export class ApiKeyService {
     })
   }
 
-  static async getApiKeysByUser(userId: number) {
-    return await ApiKey.query()
-      .where('user_id', userId)
-      .select('exchangeId', 'apiKey', 'createdAt', 'id')
+  static async getApiKeysByUser(
+    userId: number,
+    includeSecret: boolean = false
+  ): Promise<ApiKeyPick[]> {
+    const query = ApiKey.query().where('user_id', userId)
+
+    if (includeSecret) {
+      query.select('id', 'exchangeId', 'apiKey', 'createdAt', 'secret')
+    } else {
+      query.select('id', 'exchangeId', 'apiKey', 'createdAt')
+    }
+
+    const apiKeys = await query
+
+    if (includeSecret) {
+      return this.decryptApiKey(apiKeys.map((apiKey) => apiKey.toJSON() as ApiKeyPick))
+    }
+    return apiKeys
+  }
+
+  public static decryptApiKey(data: ApiKeyPick[]) {
+    return data.map((d) => ({
+      ...d,
+      secret: encryption.decrypt(d.secret) as string,
+    }))
   }
 
   static async deleteApiKey(apiKeyId: string, userId: number) {
